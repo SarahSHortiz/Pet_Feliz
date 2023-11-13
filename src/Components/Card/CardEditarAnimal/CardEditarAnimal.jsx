b309d811deb18aeed217548c565b4079ce37b6ef
Sarah <ssantoshortizr@gmail.com>
2023-11-13 19:07:02 -0300

    atualizacao

--- src/Components/Card/CardEditarAnimal/CardEditarAnimal.jsx
+++ src/Components/Card/CardEditarAnimal/CardEditarAnimal.jsx
@@ -1,39 +1,69 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import axios from "axios"; // Importe o Axios
+import PropTypes from 'prop-types';
+import './CardsAnimais.css';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import axios from 'axios';  
 
-// function CardEditarAnimal({ card }) {
-//     const navigate = useNavigate();
+function CardsEditarAnimal({ cardanimal }) {
+    if (!cardanimal) {
+        return null;
+    }
 
-//     function navegarEditar() {
-//         navigate("/src/Modal/PerfilAnimal", { state: { id: card.id } });
-//     }
+    const handleExcluir = () => {
+        axios.delete(`https://petfeliz.azurewebsites.net/api/PetFeliz/apagarPet/${cardanimal}`)
+            .then((response) => {
+                if (response.status === 200) {
+                    alert("Card excluído com sucesso!");
+                } else {
+                    alert("Erro ao excluir o card.");
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao excluir o card:", error);
+            });
+    };
 
-//     function handleExcluir() {
-//         axios.delete(`/${card.id}`).then((response) => {
-//             if (response.status === 200) {
-//                 alert("Card excluído com sucesso!");
-//             } else {
-//                 alert("Erro ao excluir o card.");
-//             }
-//         })
-//             .catch((error) => {
-//                 console.error("Erro ao excluir o card:", error);
-//             });
-//     }
-//     return (
-//         <div>
-//             <label>
-//                 <img src={card.imagem} style={{ width: 200, height: 200 }} alt={card.nome} />
-//             </label>
-//             <h1>{card.nome}</h1>
-//             <h3>CPF: {card.cidade}</h3>
-//             <h3>Email: {card.Estado}</h3>
+    return (
+        <div className='card-editar'>
+            <Card sx={{ maxWidth: 450 }}>
+                <CardActionArea>
+                    <CardMedia className='card-img' sx={{ height: 200 }}>
+                        <img src={cardanimal.foto_Pet} alt="Imagem do Card" style={{ objectFit: 'cover', width: '100%' }} />
+                    </CardMedia>
+                    <CardContent className='description' sx={{ marginBottom: '5%' }}>
+                        <div>
+                            <h2 style={{ marginBottom: '2%' }}>{cardanimal.status_Pet}</h2>
+                        </div>
+                        <h1 style={{ marginBottom: '5%' }}>{cardanimal.nome_Pet}</h1>
+                        <h3 style={{ marginBottom: '2%' }}>CEP: {cardanimal.logradouro.cep}</h3>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+            <div>
+                <Stack spacing={2} direction="row">
+                    <Button onClick={handleExcluir} variant="contained">Remover</Button>
+                </Stack>
+            </div>
+        </div>
+    );
+}
 
-//             <button onClick={navegarEditar}>Alterar</button>
-//             <button onClick={handleExcluir}>Excluir</button>
-//         </div>
-//     );
-// }
+CardsEditarAnimal.propTypes = {
+    cardanimal: PropTypes.shape({
+        status_Pet: PropTypes.string.isRequired,
+        nome_Pet: PropTypes.string.isRequired,
+        logradouro: PropTypes.shape({
+            sexo_Pet: PropTypes.string.isRequired,
+            cep: PropTypes.string.isRequired,
+            nome_Estado: PropTypes.string.isRequired,
+            estado: PropTypes.string.isRequired,
+        }),
+        foto_Pet: PropTypes.string.isRequired,
+    }).isRequired,
+};
 
-// export default CardEditarAnimal;
+export default CardsEditarAnimal;
--- src/Pages/AnimaisCadastrados/AnimaisCadastrados.jsx
+++ src/Pages/AnimaisCadastrados/AnimaisCadastrados.jsx
@@ -1,15 +1,36 @@
-
-import "../AnimaisCadastrados/AnimaisCadastrados.css"
+import { useEffect, useState } from "react";
+import "../AnimaisCadastrados/AnimaisCadastrados.css";
+import CardsEditarAnimal from "../../Components/Card/CardEditarAnimal/CardEditarAnimal";
 
 export default function AnimaisCadastrados() {
-    return (
-        <div className="body-pets-listacadastrados">
-            <div className="pets-listacadastrados-container">
-                <div className="title-listacadastrados">
-                    <h1>Animais Cadastrados</h1>
-                </div>
-                
-            </div>
+  const [animais, setAnimais] = useState([]); // Criar estado para armazenar os animais
+
+  useEffect(() => {
+    fetch("https://petfeliz.azurewebsites.net/api/PetFeliz/ListarPet", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setAnimais(json); // Atualizar o estado com os animais obtidos
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Erro ao buscar animais");
+      });
+  }, []);
+
+  return (
+    <div className="body-pets-listacadastrados">
+      <div className="pets-listacadastrados-container">
+        <div className="title-listacadastrados">
+          <h1>Animais Cadastrados</h1>
         </div>
-    )
+        <div className="cards-container">
+          {animais.map((animal) => (
+            <CardsEditarAnimal cardanimal={animal} key={animal.id_Pet} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
