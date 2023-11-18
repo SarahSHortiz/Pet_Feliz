9783024e5996108c1f9e3d2c3bdd6f3a3a3bfdb1
Sarah <ssantoshortizr@gmail.com>
2023-11-18 20:53:14 -0300

    checagem do erro com usuario no modal

--- src/Components/Card/CardsAnimal/CardsAnimal.jsx
+++ src/Components/Card/CardsAnimal/CardsAnimal.jsx
@@ -13,6 +13,10 @@ import { CardActionArea } from '@mui/material';
 function CardsAnimal({ cardanimal }) {
   const [open, setOpen] = React.useState(false);
 
+  // eslint-disable-next-line react/prop-types
+  const telefoneUsuario = cardanimal.usuario.telefone;
+  const mensagemPadrao = "%20Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20pet%20" + cardanimal.nome_Pet;
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
@@ -53,30 +57,50 @@ function CardsAnimal({ cardanimal }) {
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
             {cardanimal.porte_Pet}
-            </Typography>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
             {cardanimal.sexo_Pet}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          {cardanimal.raca.nome_Raca}
+            {cardanimal.raca.nome_Raca}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          {cardanimal.especie.nome_Especie}
+            {cardanimal.especie.nome_Especie}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          {cardanimal.idade_Pet}
+            {cardanimal.idade_Pet}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          {cardanimal.animal.nome_Animal}
+            {cardanimal.animal.nome_Animal}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          {cardanimal.Castrado}
+            {cardanimal.Castrado}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          {cardanimal.vacina}
+            {cardanimal.vacina}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
           </Typography>
+          <div>
+            <a target="_blank" href={`https://api.whatsapp.com/send?phone=${telefoneUsuario}&text=${mensagemPadrao}`} rel="noreferrer">Whatsapp</a>
+          </div>
+          <div>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          {cardanimal.cidade.nome_Cidade}
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          {cardanimal.estado.nome_Estado}
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          {cardanimal.usuario.nome}
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          {cardanimal.usuario.email}
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          {cardanimal.usuario.celular}
+          </Typography>
+          </div>
         </Box>
       </Modal>
     </>
@@ -104,26 +128,32 @@ CardsAnimal.propTypes = {
       nome_Raca: PropTypes.string.isRequired,
     }).isRequired,
     especie: PropTypes.shape({
-     nome_Especie: PropTypes.string.isRequired,
+      nome_Especie: PropTypes.string.isRequired,
     }).isRequired,
 
-    porte_Pet: PropTypes.string.isRequired,  
-    sexo_Pet: PropTypes.string.isRequired, 
+    porte_Pet: PropTypes.string.isRequired,
+    sexo_Pet: PropTypes.string.isRequired,
     nome_Raca: PropTypes.string.isRequired,
     idade_Pet: PropTypes.number.isRequired,
 
     animal: PropTypes.shape({
       nome_Animal: PropTypes.string.isRequired,
-     }).isRequired,
+    }).isRequired,
 
-  
+    usuario: PropTypes.shape({
+      nome: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      celular: PropTypes.string.isRequired,
+    }).isRequired,
+
+
   }).isRequired,
 };
 
 
 const style = {
   position: 'absolute',
-  
+
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
